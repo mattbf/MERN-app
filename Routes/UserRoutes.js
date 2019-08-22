664f613b0f1e4e5679dce3fb09ae34367241f6e5
mattbf <matthewbf8@gmail.com>
2019-08-22 11:06:46 -0600

    Added admin get all users.. will secure after

--- Routes/UserRoutes.js
+++ Routes/UserRoutes.js
@@ -107,20 +107,13 @@ router.get('/logout', function (req, res, next) {
 
 // GET admin page
 router.get('/admin', function (req, res, next) {
-  User.findById(req.session.userId)
-    .exec(function (error, user) {
-      if (error) {
-        return next(error);
-      } else {
-        if (user === null) {
-          var err = new Error('Not authorized! Go back!');
-          err.status = 400;
-          return next(err);
-        } else {
-          return res.send('<h1>Name: </h1>' + user.username + '<h2>Mail: </h2>' + user.email + '<br><a type="button" href="/logout">Logout</a>')
-        }
-      }
-    });
+  User.find(function(err, users) {
+    if (err) {
+      console.log(err)
+    } else {
+      res.json (users) //unsecure!!!
+    }
+  })
 });
 
 
