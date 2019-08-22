3021fceece2f1cad63cac3f6da784d9569642a9f
mattbf <matthewbf8@gmail.com>
2019-08-22 08:55:35 -0600

    added logic to store use rsession data:

--- server.js
+++ server.js
@@ -3,12 +3,15 @@ const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
-//const allRoutes = express.Router();
-const PrettyUrl = require('./Utils/PrettyUrl');
 const PORT = 4000;
-// let Todo = require('./todo.model');
-// let Article = require('../Models/article.model');
+
 var articleRouter = require('./Routes/ArticleRoutes');
+var userRouter = require('./Routes/UserRoutes')
+
+//user session pakckages
+var session = require('express-session');
+var MongoStore = require('connect-mongo')(session);
+
 app.use(cors());
 app.use(bodyParser.json());
 
@@ -18,56 +21,19 @@ connection.once('open', function() {
     console.log("MongoDB database connection established successfully");
 })
 
-app.use('/articles', articleRouter);
+//use sessions for tracking logins
+app.use(session({
+  secret: 'work hard',
+  resave: true,
+  saveUninitialized: false,
+  store: new MongoStore({
+    mongooseConnection: db
+  })
+}));
 
-// //TODOS
-// allRoutes.route('/').get(function(req, res) {
-//     Todo.find(function(err, todos) {
-//         if (err) {
-//             console.log(err);
-//         } else {
-//             res.json(todos);
-//         }
-//     });
-// });
-// allRoutes.route('/:id').get(function(req, res) {
-//     let id = req.params.id;
-//     Todo.findById(id, function(err, todo) {
-//         res.json(todo);
-//     });
-// });
-// allRoutes.route('/update/:id').post(function(req, res) {
-//     Todo.findById(req.params.id, function(err, todo) {
-//         if (!todo)
-//             res.status(404).send("data is not found");
-//         else
-//             todo.todo_description = req.body.todo_description;
-//             todo.todo_responsible = req.body.todo_responsible;
-//             todo.todo_priority = req.body.todo_priority;
-//             todo.todo_completed = req.body.todo_completed;
-//             todo.save().then(todo => {
-//                 res.json('Todo updated!');
-//             })
-//             .catch(err => {
-//                 res.status(400).send("Update not possible");
-//             });
-//     });
-// });
-// allRoutes.route('/add').post(function(req, res) {
-//     let todo = new Todo(req.body);
-//     todo.save()
-//         .then(todo => {
-//             res.status(200).json(
-//               {
-//                 'todo': 'todo added successfully',
-//                 'body': todo
-//             });
-//         })
-//         .catch(err => {
-//             res.status(400).send('adding new todo failed');
-//         });
-// });
-// app.use('/todos', allRoutes);
+app.use('/articles', articleRouter);
+app.use('/user', userRouter);
+
 
 
 app.listen(PORT, function() {
