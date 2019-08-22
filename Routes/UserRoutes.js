1ec04e49b985022a350946bda7cd59fa6183e927
mattbf <matthewbf8@gmail.com>
2019-08-22 09:48:25 -0600

    removed password confirmation field

--- Routes/UserRoutes.js
+++ Routes/UserRoutes.js
@@ -3,26 +3,18 @@ var router = express.Router();
 var User = require('../models/user');
 
 
-// GET route for reading data
-router.get('/', function (req, res, next) {
-  return res.sendFile(path.join(__dirname + '/templateLogReg/index.html'));
-});
+// // Don't need to display
+// router.get('/', function (req, res, next) {
+//   return res.sendFile(path.join(__dirname + '/templateLogReg/index.html'));
+// });
 
 
 //POST route for updating data
 router.post('/', function (req, res, next) {
-  // confirm that user typed same password twice
-  if (req.body.password !== req.body.passwordConf) {
-    var err = new Error('Passwords do not match.');
-    err.status = 400;
-    res.send("passwords dont match");
-    return next(err);
-  }
-
+  
   if (req.body.email &&
     req.body.username &&
-    req.body.password &&
-    req.body.passwordConf) {
+    req.body.password &&) {
 
     var userData = {
       email: req.body.email,
